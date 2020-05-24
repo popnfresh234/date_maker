81fcec18702906d5f8e4bd7aa4fdfb6b57a8f2ec
Alex <ajhollid@gmail.com>
2020-05-24 08:41:39 -0700

    clean up module

--- date_maker.js
+++ date_maker.js
@@ -1,17 +1,5 @@
 const ONE_WEEK = 7;
 
-// weeksToLookBack:  0 for current week
-const getWeekNWeeksAgo = ( n ) => {
-  const currentDate = new Date();
-  const dateArr = [];
-  const startingDate = new Date( new Date().setDate( currentDate.getDate() - ( n * ONE_WEEK ) ) );
-  for ( let i = 0; i < ONE_WEEK; i += 1 ) {
-    dateArr.push( new Date( new Date().setDate( startingDate.getDate() - i ) ) );
-  }
-  return dateArr;
-};
-
-
 const getMonth = ( date ) => {
   let month = date.getMonth() + 1;
   if ( month < 10 ) {
@@ -20,20 +8,36 @@ const getMonth = ( date ) => {
   return month;
 };
 
-const getDateNDaysAgo = n => new Date( new Date().setDate( new Date().getDate() - n ) );
-
-const getTwentyFourHourPeriodFromDate = ( date ) => {
+const getStartAndEndTimes = ( startDate, endDate ) => {
   // 2020-05-24T02:48:01-08:00
-  const offset = date.getTimezoneOffset() / 60;
-  const startTime = `${date.getFullYear()}-${getMonth( date )}-${date.getDate()}T00:00:00-0${offset}:00`;
-  const endTime = `${date.getFullYear()}-${getMonth( date )}-${date.getDate()}T23:59:59-0${offset}:00`;
+  if ( !endDate ) endDate = new Date( startDate );
+  const startOffset = startDate.getTimezoneOffset() / 60;
+  const endOffset = endDate.getTimezoneOffset() / 60;
+  const endTime = `${startDate.getFullYear()}-${getMonth( startDate )}-${startDate.getDate()}T00%3A00%3A00-0${startOffset}%3A00`;
+  const startTime = `${endDate.getFullYear()}-${getMonth( endDate )}-${endDate.getDate()}T23%3A59%3A59-0${endOffset}%3A00`;
   return { startTime, endTime };
 };
 
-console.log( getWeekNWeeksAgo( 1 ) );
+
+// weeksToLookBack:  0 for current week
+const getWeekRangeNWeeksAgo = ( n ) => {
+  const currentDate = new Date();
+  const startDate = new Date( new Date().setDate( currentDate.getDate() - ( n * ONE_WEEK ) ) );
+  const endDate = new Date( new Date().setDate( startDate.getDate() - 6 ) );
+  return getStartAndEndTimes( startDate, endDate );
+};
+
+
+const getDayRangeNDaysAgo = ( n ) => {
+  const date = new Date( new Date().setDate( new Date().getDate() - n ) );
+  return getStartAndEndTimes( date );
+};
+
+
+// console.log( getStartAndEndTimes( new Date() ) );
+console.log( getDayRangeNDaysAgo( 1 ) );
 
 module.exports = {
-  getWeekNWeeksAgo,
-  getTwentyFourHourPeriodFromDate,
-  getDateNDaysAgo,
+  getWeekRangeNWeeksAgo,
+  getDayRangeNDaysAgo,
 };
