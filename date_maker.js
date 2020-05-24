875ee62079b33fbbf523abbc5765e9ceb26ced8e
Alex <ajollid@gmail.com>
2020-05-23 19:13:09 -0800

    start twenty four hour period function

--- date_maker.js
+++ date_maker.js
@@ -22,6 +22,34 @@ const getSameDayNDaysAgo = ( n ) => {
   return arr;
 };
 
+const getMonth = ( date ) => {
+  let month = date.getMonth() + 1;
+  if ( month < 10 ) {
+    month = `0${month}`;
+  }
+  return month;
+};
+
+const getTwentyFourHourPeriod = ( currentDate, daysAgo ) => {
+  // 2020-05-24T02:48:01-08:00
+  const targetDate = new Date().setDate( currentDate.getDate() - daysAgo );
+  const offset = currentDate.getTimezoneOffset() / 60;
+  console.log( targetDate );
+
+  const startTime = `${currentDate.getFullYear()}-${getMonth( currentDate )}-${currentDate.getDate()}T00:00:00-0${offset}:00`;
+  const endTime = `${currentDate.getFullYear()}-${getMonth( currentDate )}-${currentDate.getDate()}T23:59:59-0${offset}:00`;
+
+  // const targetStartTime = `${targetDate.getFullYear()}-${getMonth( targetDate )}-${targetDate.getDate()}T00:00:00-0${offset}:00`;
+  // const targetEndTime = `${currentDate.getFullYear()}-${getMonth( targetDate )}-${targetDate.getDate()}T23:59:59-0${offset}:00`;
+
+  console.log( startTime, endTime );
+  // console.log( targetStartTime, targetEndTime );
+};
+
+
+// getTwentyFourHourPeriod( new Date(), 0 );
+getTwentyFourHourPeriod( new Date(), 1 );
+
 
 module.exports = {
   getWeekNWeeksAgo,
